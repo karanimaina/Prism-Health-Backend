c345b93d4c7c93f1c3ac0d59756e95cb5bea7e08
karanimaina <karani.maina2010@gmail.com>
2022-12-21 17:48:57 +0300

    first commit

---
+++ src/test/java/com/arcurus/prism/PrismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcurus.prism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
