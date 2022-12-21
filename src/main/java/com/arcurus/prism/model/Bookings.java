0d3758f8038f294f685aa63ca1d5da7457aa82b1
karanimaina <karani.maina2010@gmail.com>
2022-12-21 18:28:44 +0300

    add booking

---
+++ src/main/java/com/arcurus/prism/model/Bookings.java
@@ -0,0 +1,19 @@
+package com.arcurus.prism.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+public class Bookings {
+    @Id
+    private String id;
+    private String serviceId;
+    private String userPhone;
+    private String date;
+    private int hour;
+    private boolean cancelled;
+    private Date timestamp;
+    private Users user;
+    private Services service;
+
+}
