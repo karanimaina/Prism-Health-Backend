2bb4976a5ad5713d14ce1c093ed49ae8e47909fd
karanimaina <karani.maina2010@gmail.com>
2022-12-21 18:39:48 +0300

    add email details

---
+++ src/main/java/com/arcurus/prism/model/EmailDetails.java
@@ -0,0 +1,12 @@
+package com.arcurus.prism.model;
+
+import java.util.Date;
+
+public class EmailDetails {
+    private String carName;
+    private String location;
+    private Date date;
+    private Date returnBy;
+    private String phone;
+    private Date returnDate;
+}
