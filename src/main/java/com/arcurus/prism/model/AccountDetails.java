cea618e13de57f1e35a9bd56b4d4c65dc17279f3
karanimaina <karani.maina2010@gmail.com>
2022-12-21 18:20:47 +0300

    add account details and accessors"

--- src/main/java/com/arcurus/prism/model/AccountDetails.java
+++ src/main/java/com/arcurus/prism/model/AccountDetails.java
@@ -1,4 +1,12 @@
 package com.arcurus.prism.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AccountDetails {
+    private String email;
+    private String accesstoken;
+    private String username;
 }
